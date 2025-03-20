7ae3d3b73037ac6819257147a47007fabb836e10
unknown <edher_suriel@hotmail.com>
2025-03-20 12:38:14 -0600

    ejercicios semana 2 ESNC

---
+++ POO/Interfaces/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Objeto que contiene los metodos propios de la clase
+        Chef ch = new Chef("Luigi", "cocina romana");
+
+        ch.cortarVejetales();
+        ch.prepararMasa();
+        ch.freirComida();
+
+
+
+    }
+}
