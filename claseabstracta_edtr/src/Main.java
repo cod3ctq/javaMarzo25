9f1ef459b06d46beb6889cf54972f5578f686071
eddhalai <53988084+eddhalai@users.noreply.github.com>
2025-03-20 18:05:19 -0600

    Update Main.java

--- claseabstracta_edtr/src/Main.java
+++ claseabstracta_edtr/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Selecciona el asciento: ");
+        int asc1 = sc.nextInt();
+        System.out.println("Selecciona el tipo de pago (efectivo o tarjeta: ");
+        String typeBuy1 = sc.next();
+        System.out.println("Ingresa tu tarjeta debito o credito: ");
+        String cardNumber1= sc.next();
+
+        Tapo tap= new Tapo();
+        tap.venderBoletoFisico(asc1,typeBuy1);
+        tap.venderBoletoOnline(cardNumber1, asc1);
+
+        System.out.println("Selecciona el asciento: ");
+        int asc2 = sc.nextInt();
+        System.out.println("Selecciona el tipo de pago (efectivo o tarjeta: ");
+        String typeBuy2 = sc.next();
+        System.out.println("Ingresa tu tarjeta debito o credito: ");
+        String cardNumber2= sc.next();
+
+        Aeropuerto aero = new Aeropuerto();
+        aero.venderBoletoFisico(asc2,typeBuy2);
+        aero.venderBoletoOnline(cardNumber2, asc2);
+
+        System.out.println(tap);
+        System.out.println(aero);
     }
 }
